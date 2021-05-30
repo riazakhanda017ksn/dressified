a2a6184e2b720284f4f7cd30cc0eaf43e20f9f98
Raisul Islam Nibir <raisulislam.cse3.bu@gmail.com>
2021-05-30 15:54:06 +0600

    comment added

--- src/containers/Header/__test__/Header.test.js
+++ src/containers/Header/__test__/Header.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+//unit test code
 it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(<span></span>, div)
